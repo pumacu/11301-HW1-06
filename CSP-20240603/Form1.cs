b40c69101c929df39f9fbd51eb124b70256bfa8a
pumacu <pumacu@yahoo.com.tw>
2024-06-03 11:23:26 +0800

    init ver

---
+++ CSP-20240603/Form1.cs
@@ -0,0 +1,10 @@
+namespace CSP_20240603
+{
+      public partial class Form1 : Form
+      {
+            public Form1()
+            {
+                  InitializeComponent();
+            }
+      }
+}
